30cf8716c3df08a2c00997d95cc1ded062423650
VAINKTESH SINGH <iamvs9587@gmail.com>
2024-08-13 10:41:37 +0530

    Practice

---
+++ java_practice/question09.java
@@ -0,0 +1,16 @@
+package java_practice;
+import java.util.*;
+public class question09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number 1");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter number 2");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("The sum is" + " " + sum);
+    }
+}
---
+++ java_practice/question10.java
@@ -0,0 +1,15 @@
+package java_practice;
+
+import java.util.Scanner;
+
+public class question10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your Name");
+        String name = sc.nextLine();
+
+        System.out.println("Hello" +  name   + "Have a good day");
+
+    }
+}
