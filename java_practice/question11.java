278da0e259b12698cd427babe0b4852ec4378d59
VAINKTESH SINGH <iamvs9587@gmail.com>
2024-08-13 10:44:35 +0530

    Practice

---
+++ java_practice/question11.java
@@ -0,0 +1,19 @@
+package java_practice;
+
+public class question11 {
+    public static void main(String[] args) {
+        
+        //Swapping numbers
+
+        int a = 10; 
+        int b = 20;
+
+        int swap = a;
+        a=b;
+        b=swap;
+        System.out.println(a);
+        System.out.println(b);
+        
+
+    }
+}
