9a859799ebdb0acd43a2f7a3f127839116169dd2
VAINKTESH SINGH <iamvs9587@gmail.com>
2024-08-13 10:27:58 +0530

    Strings

---
+++ Java_strings/strings01.java
@@ -0,0 +1,10 @@
+package Java_strings;
+
+public class strings01 {
+    public static void main(String[] args) {
+        String name = "VAINKTESH SINGH";
+        System.out.println(name);
+
+      
+    }
+}
---
+++ Java_strings/strings_methods.java
@@ -0,0 +1,20 @@
+package Java_strings;
+
+public class strings_methods {
+    public static void main(String[] args) {
+        String name = "Vainktesh Singh";
+
+        //Method 1 (converting to lower case)
+
+        String lcase = name.toLowerCase();
+        System.out.println(lcase);
+
+        //Method 2 (Convering to Upper case)
+        String ucase = name.toUpperCase();
+        System.out.println(ucase);
+
+        //Method 3 (Find the number of characters)
+        int value = name.length();
+        System.out.println(value);
+    }
+}
